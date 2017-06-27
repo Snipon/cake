9485b5954c4f32d2c7297ff6e413fc432c345958
Simon Larsson <me@simon-larsson.se>
2017-06-27 22:20:29 +0200

    Initial Commit

---
+++ CakeTests/CakeTests.swift
@@ -0,0 +1,36 @@
+//
+//  CakeTests.swift
+//  CakeTests
+//
+//  Created by Simon Larsson on 2017-06-27.
+//  Copyright © 2017 Simon Larsson. All rights reserved.
+//
+
+import XCTest
+@testable import Cake
+
+class CakeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
